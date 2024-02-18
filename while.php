2c4c2ad2af08ab5adbca5f959fceabdcdf694b1f
Mohammad Mumunur Rashid <115931863+mamunPrince@users.noreply.github.com>
2024-02-18 11:59:24 +0600

    add via upload
    important for wordpress developing

---
+++ while.php
@@ -0,0 +1,15 @@
+<?php
+    $starting_video = 0;
+    while( $starting_video <= 15 ){
+        echo "watching going on $starting_video<br>";
+        $starting_video+=1;
+
+    }
+    echo "finish<br><br>";
+    $post = 15; $init = 0;
+    while( $init<= $post ){
+        echo "The while formula for any post calculation".$init."<br>";
+        $init++;
+    }
+
+?>
