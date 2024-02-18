9751fb346a23fbdaef7f8681168814b8cf51be1d
Mohammad Mumunur Rashid <115931863+mamunPrince@users.noreply.github.com>
2024-02-18 12:22:00 +0600

    add array file via upload
    output of an array value

---
+++ array.php
@@ -0,0 +1,8 @@
+<?php
+    $info = array("Mamun","01881164650","01790011223");
+    $count_array_value = count($info);
+    for ($i = 0; $i < $count_array_value; $i++){
+        echo "<br>".$info[$i]."<br>";
+    }
+
+?>
